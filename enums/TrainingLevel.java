6d12491d98f992280e3ba628788fe3e971e240dd
38691-max <38691@raphaeldisanto.com.br>
2026-05-18 08:58:18 -0300

    Add files via upload

--- enums/TrainingLevel.java
+++ enums/TrainingLevel.java
@@ -1,5 +1,27 @@
 package enums;
 
-public class TrainingLevel {
+public enum TrainingLevel {
+    BEGINNER(1, "Beginner"),
+    INTERMEDIATE(2, "rgetger"),
+    ADVANCED(3, "advanced"),
+    PROFESSIONAL(4, "Professional");
+
+    private final int code;
+    private final String displayName;
+
+    TrainingLevel(int code, String displayName) {
+        this.code = code;
+        this.displayName = displayName;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
+
     
+
 }
--- model/Dog.java
+++ model/Dog.java
@@ -1,3 +1,19 @@
-public class Dog {
-    
+package model;
+import model.Animal;
+import enums.Habitat;
+import enums.TrainingLevel;
+
+public class Dog extends Animal {
+    private String breed;
+    private boolean isVaccinated;
+    private TrainingLevel trainingLevel;
+    private String ownerName;
+    private int barkVolume;
+
+    public Dog(String name, int age, double weight, String breed, boolean isVaccinated, TrainingLevel trainingLevel, String ownerName, int barkVolume) {
+        super(name, speed:48, age, weight, Habitat.DOMESTIC, isWild: false);
+        this.breed = breed;
+        this.isVaccinated = isVaccinated;
+        
+    }
 }
